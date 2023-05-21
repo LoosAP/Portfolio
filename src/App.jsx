5a72b8a1efd03ca3d18c1e52ded06efdbf9cb3db
Loós András Péter <loosandraspeter@gmail.com>
2023-05-21 11:16:11 +0200

    developed routing to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { Footer, Home, Navbar, Projects } from "./components";
+import {
+  Contact,
+  Error404,
+  Footer,
+  Home,
+  Navbar,
+  Projects,
+} from "./components";
 import styles from "./style";
 
 const App = () => {
   return (
-    <div className="w-full overflow-hidden bg-primary">
+    <div className="w-full min-h-screen overflow-hidden bg-primary">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
           <Navbar />
@@ -14,6 +21,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Error404 />} />
       </Routes>
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
