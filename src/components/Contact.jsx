c1951d1a734fbe09c0297dae10e93c3b2935a3cc
Loós András Péter <loosandraspeter@gmail.com>
2023-05-21 13:10:52 +0200

    added Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { socialMedia } from "../consts";
+import styles from "../style";
+import ContactCard from "./ContactCard";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section
+      className={`${styles.padding} ${styles.flexCenter}  flex-col relative`}
+    >
+      <div className="flex flex-wrap justify-center w-full feedback-container">
+        {socialMedia.map((card) => (
+          <ContactCard key={card.id} {...card} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
