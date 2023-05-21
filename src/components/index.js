b7d33d6944a29b25f088542e372ba474fb6d2f67
Loós András Péter <loosandraspeter@gmail.com>
2023-05-21 20:01:29 +0200

    removed unused Components

--- src/components/index.js
+++ src/components/index.js
@@ -1,18 +1,15 @@
-import Billing from "./Billing";
 import Button from "./Button";
 import CTA from "./CTA";
-import CardDeal from "./CardDeal";
 
 import Contact from "./Contact";
 import Error404 from "./Error404";
-import FeedbackCard from "./FeedbackCard";
+
 import Footer from "./Footer";
 import GetInContact from "./GetInContact";
 import Hero from "./Hero";
 import Home from "./Home";
 import Navbar from "./Navbar";
 import Projects from "./Projects";
-import Testimonials from "./Testimonials";
 import Tools from "./Tools";
 import WhoAmI from "./WhoAmI";
 
@@ -24,13 +21,9 @@ export {
   Home,
   Projects,
   Navbar,
-  Billing,
-  CardDeal,
   CTA,
   Footer,
-  Testimonials,
   Hero,
   GetInContact,
-  FeedbackCard,
   Button,
 };
