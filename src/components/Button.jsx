0203fdb6b54260a0ea4784ac8d532a31629e63f1
Loós András Péter <loosandraspeter@gmail.com>
2023-11-07 10:51:14 +0100

    removed CV button for web scraping security reason

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -12,14 +12,14 @@ const Button = ({ styles }) => {
           Get In Contact
         </button>
       </Link>
-      <a href="../assets/Loós András Péter Önéletrajz.pdf" target="_blank">
+      {/* <a href="../assets/Loós András Péter Önéletrajz.pdf" target="_blank">
         <button
           type="button"
           className={`mt-3 py-4 px-6 bg-pink-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px] ease-in-out duration-300 hover:shadow-xl hover:scale-105`}
         >
           Download my CV
         </button>
-      </a>
+      </a> */}
     </section>
   );
 };
